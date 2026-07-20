46cfba597a04056faef91afbb81cb92f8f2917df
Vaight <109642180+Vaight@users.noreply.github.com>
2026-07-20 02:23:58 -0600

    fix: update linux VST3 path

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -25,7 +25,7 @@ fi
 
 cmake "${build_args[@]}"
 
-VST3_PATH="$BUILD_DIR/ohmsick_vst3_artefacts/$CONFIG/VST3/Ohmsick.vst3"
+VST3_PATH="$BUILD_DIR/ohmsick_plugin_artefacts/$CONFIG/VST3/Ohmsick.vst3"
 if [[ -d "$VST3_PATH" ]]; then
     printf 'Built VST3: %s\n' "$VST3_PATH"
 fi
